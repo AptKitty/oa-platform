156819220b165649f1927e4aaea14fbc5b6f9b8c
OA-Team <student@oa.platform>
2026-06-19 15:02:03 +0800

    修复：Dept实体类添加toString()，部门管理树节点显示部门名称而非内存地址

--- src/main/java/com/oa/system/entity/Dept.java
+++ src/main/java/com/oa/system/entity/Dept.java
@@ -22,4 +22,9 @@ public class Dept extends BaseEntity {
     public void setSortOrder(Integer sortOrder) { this.sortOrder = sortOrder; }
     public Integer getStatus() { return status; }
     public void setStatus(Integer status) { this.status = status; }
+
+    @Override
+    public String toString() {
+        return deptName != null ? deptName : "(未命名)";
+    }
 }
